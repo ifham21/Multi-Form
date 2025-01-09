3e66add583703e5425c476b5df28039d08360828
Ahmed Ifham <ifham.maa@gmail.com>
2025-01-10 00:10:44 +0530

    Updates

--- server/user-service/src/main/java/com/user/repository/UserRepository.java
+++ server/user-service/src/main/java/com/user/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.user.repository;
 
-public class UserRepository {
+import com.user.document.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, Long> {
 }
