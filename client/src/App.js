76d0569a7ea349cda31177557db376f257127808
Ahmed Ifham <ifham.maa@gmail.com>
2025-01-11 21:40:49 +0530

    Updates

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login/Login';
 import RegisterPage from './Register/RegisterPage';
-import MultiStepForm from './Register/ClientRegister/MultiStepForm';
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/client-multi" element={<MultiStepForm/>} />
       </Routes>
     </Router>
   );
